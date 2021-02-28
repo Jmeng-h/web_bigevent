ca82fbd0448259b0a81d58d916f242b1852b1f84
Jmengh- <1171151936@qq.com>
2021-02-28 21:44:14 +0800

    新修改的

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,4 @@
 $.ajaxPrefilter(function(options) {
     // console.log(options.url);
-    options.url = 'http://www.liulongbin.top:3007' + options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
 })
